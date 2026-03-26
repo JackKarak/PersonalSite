c579657b688df6866295b4b59d52235d795e7935
jkaraka <jkara@terpmail.umd.edu>
2026-03-26 11:35:44 -0400

    Harden Vercel OAuth functions and pin Node 20 runtime

--- api/begin.js
+++ api/begin.js
@@ -1,3 +1,36 @@
 const { createVercelBeginHandler } = require('netlify-cms-oauth-provider-node');
 
-module.exports = createVercelBeginHandler({}, { useEnv: true });
+const requiredEnvVars = [
+	'OAUTH_CLIENT_ID',
+	'OAUTH_CLIENT_SECRET',
+	'ORIGIN',
+	'COMPLETE_URL',
+	'OAUTH_PROVIDER'
+];
+
+const getMissingEnvVars = () => requiredEnvVars.filter((key) => !process.env[key]);
+
+module.exports = async (req, res) => {
+	const missing = getMissingEnvVars();
+	if (missing.length > 0) {
+		res.statusCode = 500;
+		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+		res.end(JSON.stringify({
+			error: 'Missing required environment variables for OAuth function.',
+			missing
+		}));
+		return;
+	}
+
+	try {
+		const handler = createVercelBeginHandler({}, { useEnv: true });
+		await handler(req, res);
+	} catch (error) {
+		res.statusCode = 500;
+		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+		res.end(JSON.stringify({
+			error: 'OAuth begin handler failed.',
+			message: error && error.message ? error.message : 'Unknown error'
+		}));
+	}
+};
